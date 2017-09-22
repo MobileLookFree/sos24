b45b391f8b4accb55fa6e8891247a1dc6a26f595
Andrei <mobilelookfree@gmail.com>
2017-09-22 21:35:34 +0300

    move to new rep

---
+++ js/init.js
@@ -0,0 +1,14 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+	$('.materialboxed').materialbox();
+	$('.dropdown-button').dropdown();
+    $('.carousel.carousel-slider').carousel({full_width: true});
+    $('.carousel').carousel();
+        
+
+
+
+  }); // end of document ready
+})(jQuery); 
